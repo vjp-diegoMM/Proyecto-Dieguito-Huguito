a085007490299da9f460eeb36ee85866d69a46d6
vjp-diegoMM <dmoralesm01@informatica.iesvalledeljerteplasencia.es>
2025-11-04 20:20:43 +0100

    Nada-04/11/2025

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <?php
+namespace Dwes\ProyectoVideoclub;
 session_start();
 
 $usuarios_validos = [
@@ -10,12 +11,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $usuario = $_POST['usuario'] ?? '';
     $contrasena = $_POST['contrasena'] ?? '';
 
-    if (isset($usuarios_validos[$usuario]) && $usuarios_validos[$usuario] === $contrasena) {
+    if (isset($usuarios_validos['usuario']) && $usuarios_validos['usuario'] === $contrasena) {
         $_SESSION['usuario'] = $usuario;
         header('Location: main.php');
-    } else {
+    } if (isset($usuarios_validos['admin']) && $usuarios_validos['admin'] === $contrasena) {
+        // $_SESSION['usuario'] = $usuario;
+        // header('Location: main.php');
+        
+    }else {
         header('Location: index.php?error=1');
     }
-} else {
-    header('Location: index.php');
 }
